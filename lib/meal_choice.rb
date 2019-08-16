edb49b7bc1d5c98344a863e7efb1fedcf4c6afd8
Alex Mendes <mendes.develop@gmail.com>
2019-08-16 13:19:00 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,7 @@
 def meal_choice(veg1, veg2, protein = "meat")
   puts "What a nutritious meal!"
   order = "A plate of #{protein} with #{veg1} and #{veg2}."
-  puts order
+  return puts order
 end
 
 meal_choice("broccolli", "macaroni")
