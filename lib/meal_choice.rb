0128ee985674663542f097b397b1051c108b7dae
Alex Mendes <mendes.develop@gmail.com>
2019-08-16 12:57:14 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,6 +6,6 @@ def meal_choice(veg1, veg2, protein = "meat")
   
 end
 
-puts meal_choice("broccolli", "macaroni")
+puts "#{meal_choice("broccolli", "macaroni")}"
 
 meal_choice("broccoli", "macaroni", "tofu")
